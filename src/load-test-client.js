cfdde96f686f431170eb849c9d7abe07ffb912bd
ekougs <ekougs@gmail.com>
2021-04-02 20:55:36 +0200

    Generate a small conversation for each user in the load test

--- src/load-test-client.js
+++ src/load-test-client.js
@@ -19,19 +19,47 @@ export let options = {
     },
 };
 
+// From MDN - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+function generateConversation(socket) {
+    const nbMessages = getRandomIntInclusive(4, 10);
+    const sendMessage = (intervalMs) => {
+        socket.send(`Message generated to be sent after ${intervalMs} ms`);
+    };
+
+    let interval = 0;
+    // Generate the n - 1 first messages to be sent async
+    for(let i = 0 ; i < nbMessages - 1 ; i++) {
+        interval += getRandomIntInclusive(200, 500);
+        // If interval is not deeply copied when the function is executed, it will always get its last value
+        const currentInterval = JSON.parse(JSON.stringify(interval));
+        socket.setTimeout(() => {
+            sendMessage(currentInterval);
+        }, interval);
+    }
+    // Generate the last message to be sent async and then close the socket
+    interval += getRandomIntInclusive(200, 500);
+    socket.setTimeout(() => {
+        sendMessage(interval);
+        socket.close()
+    }, interval);
+}
+
 export default () => {
     const url = `ws://${host}:${port}/echo`;
     const params = { tags: { my_tag: 'hello' } };
     const res = ws.connect(url, params, function (socket) {
         socket.on('open', () => {
-            // TODO send 2-5 messages after 200-500 separated by 500-1000 ms each
             console.log('connected');
-            socket.send('Hello');
+            generateConversation(socket);
         });
         socket.on('message', (data) => {
             console.log('Message received: ', data);
-            // TODO close after the last message above
-            socket.close();
         });
         socket.on('close', () => console.log('disconnected'));
     });
